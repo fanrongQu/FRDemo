2a5ab9f656b2882d6258fb4343ff6f7e05ae0036
fanrongQu <1366225686@qq.com>
2017-06-03 19:20:19 +0800

    FR

--- FRDemo/FRDemo/FRCategory/MBProgressHUD/MBProgressHUD+Category.h
+++ FRDemo/FRDemo/FRCategory/MBProgressHUD/MBProgressHUD+Category.h
@@ -8,31 +8,268 @@
 
 #import "MBProgressHUD.h"
 
+
 @interface MBProgressHUD (Category)
 
-/**
- *  提醒HUD
- *
- *  @param text       提醒标题
- *  @param detailText 副标题
- *  @param icon       图片
- *  @param color      颜色
- *  @param view       显示的view
- */
-+ (void)showTitle:(NSString *)text detailsLabel:(NSString *)detailText icon:(NSString *)icon contentColor:(UIColor *)color inView:(UIView *)view;
 
+#pragma mark - 事件
 /**
  *  带有活动指示器的HUD
  *
- *  @param text            标题
- *  @param detailText      副标题
+ *  @param title           标题
+ *  @param detailTitle     副标题
  *  @param color           颜色
  *  @param progressHUDMode 指示器类型
  *  @param view            显示的view
  *
  *  @return MBProgressHUD对象
  */
-+ (MBProgressHUD *)showTitle:(NSString *)text detailsLabel:(NSString *)detailText contentColor:(UIColor *)color progressHUDMode:(MBProgressHUDMode)progressHUDMode inView:(UIView *)view;
++ (MBProgressHUD *)showActivityIndicatorWithTitle:(NSString *)title detailsTitle:(NSString *)detailTitle contentColor:(UIColor *)color progressHUDMode:(MBProgressHUDMode)progressHUDMode inView:(UIView *)view;
+
+
+/**
+ *  带有活动指示器的HUD
+ *
+ *  @param title           标题
+ *  @param detailTitle     副标题
+ *  @param progressHUDMode 指示器类型
+ *  @param view            显示的view
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showActivityIndicatorWithTitle:(NSString *)title detailsTitle:(NSString *)detailTitle progressHUDMode:(MBProgressHUDMode)progressHUDMode inView:(UIView *)view;
+
+
+/**
+ *  带有活动指示器的HUD
+ *
+ *  @param title           标题
+ *  @param detailTitle     副标题
+ *  @param progressHUDMode 指示器类型
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showActivityIndicatorWithTitle:(NSString *)title detailsTitle:(NSString *)detailTitle progressHUDMode:(MBProgressHUDMode)progressHUDMode;
+
+
+/**
+ *  带有活动指示器的HUD
+ *
+ *  @param title           标题
+ *  @param color           颜色
+ *  @param progressHUDMode 指示器类型
+ *  @param view            显示的view
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showActivityIndicatorWithTitle:(NSString *)title contentColor:(UIColor *)color progressHUDMode:(MBProgressHUDMode)progressHUDMode inView:(UIView *)view;
+
+
+/**
+ *  带有活动指示器的HUD
+ *
+ *  @param title           标题
+ *  @param progressHUDMode 指示器类型
+ *  @param view            显示的view
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showActivityIndicatorWithTitle:(NSString *)title progressHUDMode:(MBProgressHUDMode)progressHUDMode inView:(UIView *)view;
+
+
+/**
+ *  带有活动指示器的HUD
+ *
+ *  @param title           标题
+ *  @param progressHUDMode 指示器类型
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showActivityIndicatorWithTitle:(NSString *)title progressHUDMode:(MBProgressHUDMode)progressHUDMode;
+
+#pragma mark - 动画HUD
+/**
+ *  带有自定义动画HUD的HUD
+ *
+ *  @param title       提醒标题
+ *  @param detailTitle 副标题
+ *  @param icon       图片
+ *  @param color      颜色
+ *  @param view       显示的view
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showCustomeAnimationTitle:(NSString *)title detailsLabel:(NSString *)detailTitle icon:(NSString *)icon contentColor:(UIColor *)color inView:(UIView *)view;
+
+
+
+/**
+ *  带有自定义动画HUD的HUD
+ *
+ *  @param title      提醒标题
+ *  @param icon       图片
+ *  @param color      颜色
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showCustomeAnimationTitle:(NSString *)title icon:(NSString *)icon contentColor:(UIColor *)color;
+
+#pragma mark - 常用提示状态
+
+/**
+ *  成功提醒
+ *
+ *  @param success 提醒文字
+ *  @param color   颜色
+ *  @param view    显示的view
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showSuccess:(NSString *)success contentColor:(UIColor *)color inView:(UIView *)view;
+/**
+ *  成功提醒
+ *
+ *  @param success 提醒文字
+ *  @param view    显示的view
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showSuccess:(NSString *)success inView:(UIView *)view;
+/**
+ *  成功提醒
+ *
+ *  @param success 提醒文字
+ *  @param color   颜色
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showSuccess:(NSString *)success contentColor:(UIColor *)color;
+/**
+ *  成功提醒
+ *
+ *  @param success 提醒文字
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showSuccess:(NSString *)success;
+
+
+
+/**
+ *  失败提醒
+ *
+ *  @param error   提醒文字
+ *  @param color   颜色
+ *  @param view    显示的view
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showError:(NSString *)error contentColor:(UIColor *)color inView:(UIView *)view;
+/**
+ *  失败提醒
+ *
+ *  @param error   提醒文字
+ *  @param view    显示的view
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showError:(NSString *)error inView:(UIView *)view;
+/**
+ *  失败提醒
+ *
+ *  @param error   提醒文字
+ *  @param color   颜色
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showError:(NSString *)error contentColor:(UIColor *)color;
+/**
+ *  失败提醒
+ *
+ *  @param error   提醒文字
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showError:(NSString *)error;
+
+
+/**
+ *  警告提醒
+ *
+ *  @param info    提醒文字
+ *  @param color   颜色
+ *  @param view    显示的view
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showInfo:(NSString *)info contentColor:(UIColor *)color inView:(UIView *)view;
+/**
+ *  警告提醒
+ *
+ *  @param info    提醒文字
+ *  @param view    显示的view
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showInfo:(NSString *)info inView:(UIView *)view;
+/**
+ *  警告提醒
+ *
+ *  @param info    提醒文字
+ *  @param color   颜色
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showInfo:(NSString *)info contentColor:(UIColor *)color;
+/**
+ *  警告提醒
+ *
+ *  @param info 提醒文字
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showInfo:(NSString *)info;
+
+
+/**
+ *  提醒
+ *
+ *  @param message 提醒文字
+ *  @param color   颜色
+ *  @param view    显示的view
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showMessage:(NSString *)message contentColor:(UIColor *)color inView:(UIView *)view;
+/**
+ *  提醒
+ *
+ *  @param success 提醒文字
+ *  @param view    显示的view
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showMessage:(NSString *)success inView:(UIView *)view;
+/**
+ *  提醒
+ *
+ *  @param success 提醒文字
+ *  @param color   颜色
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showMessage:(NSString *)success contentColor:(UIColor *)color;
+/**
+ *  提醒
+ *
+ *  @param success 提醒文字
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showMessage:(NSString *)success;
+
+
+#pragma mark - 隐藏HUD
 /**
  *  隐藏HUD
  *
@@ -43,200 +280,6 @@
  *  隐藏HUD
  */
 + (void)hideHUD;
-
-#pragma mark - 事件
-/**
- *  带有活动指示器的HUD
- *
- *  @param text            标题
- *  @param detailText      副标题
- *  @param color           颜色
- *  @param progressHUDMode 指示器类型
- *  @param view            显示的view
- *
- *  @return MBProgressHUD对象
- */
-+ (MBProgressHUD *)showActivityIndicatorWithTitle:(NSString *)title detailsTitle:(NSString *)detailTitle contentColor:(UIColor *)color progressHUDMode:(MBProgressHUDMode)progressHUDMode inView:(UIView *)view;
-
-/**
- *  带有活动指示器的HUD
- *
- *  @param text            标题
- *  @param detailText      副标题
- *  @param progressHUDMode 指示器类型
- *  @param view            显示的view
- *
- *  @return MBProgressHUD对象
- */
-+ (MBProgressHUD *)showActivityIndicatorWithTitle:(NSString *)title detailsTitle:(NSString *)detailTitle progressHUDMode:(MBProgressHUDMode)progressHUDMode inView:(UIView *)view;
-
-/**
- *  带有活动指示器的HUD
- *
- *  @param text            标题
- *  @param detailText      副标题
- *  @param progressHUDMode 指示器类型
- *
- *  @return MBProgressHUD对象
- */
-+ (MBProgressHUD *)showActivityIndicatorWithTitle:(NSString *)title detailsTitle:(NSString *)detailTitle progressHUDMode:(MBProgressHUDMode)progressHUDMode;
-
-/**
- *  带有活动指示器的HUD
- *
- *  @param text            标题
- *  @param color           颜色
- *  @param progressHUDMode 指示器类型
- *  @param view            显示的view
- *
- *  @return MBProgressHUD对象
- */
-+ (MBProgressHUD *)showActivityIndicatorWithTitle:(NSString *)title contentColor:(UIColor *)color progressHUDMode:(MBProgressHUDMode)progressHUDMode inView:(UIView *)view;
-
-/**
- *  带有活动指示器的HUD
- *
- *  @param text            标题
- *  @param progressHUDMode 指示器类型
- *  @param view            显示的view
- *
- *  @return MBProgressHUD对象
- */
-+ (MBProgressHUD *)showActivityIndicatorWithTitle:(NSString *)title progressHUDMode:(MBProgressHUDMode)progressHUDMode inView:(UIView *)view;
-
-/**
- *  带有活动指示器的HUD
- *
- *  @param text            标题
- *  @param progressHUDMode 指示器类型
- *
- *  @return MBProgressHUD对象
- */
-+ (MBProgressHUD *)showActivityIndicatorWithTitle:(NSString *)title progressHUDMode:(MBProgressHUDMode)progressHUDMode;
-
-#pragma mark - 提示状态
-
-/**
- *  成功提醒
- *
- *  @param success 提醒文字
- *  @param color   颜色
- *  @param view    显示的view
- */
-+ (void)showSuccess:(NSString *)success contentColor:(UIColor *)color inView:(UIView *)view;
-/**
- *  成功提醒
- *
- *  @param success 提醒文字
- *  @param view    显示的view
- */
-+ (void)showSuccess:(NSString *)success inView:(UIView *)view;
-/**
- *  成功提醒
- *
- *  @param success 提醒文字
- *  @param color   颜色
- */
-+ (void)showSuccess:(NSString *)success contentColor:(UIColor *)color;
-/**
- *  成功提醒
- *
- *  @param success 提醒文字
- */
-+ (void)showSuccess:(NSString *)success;
-
-
-
-/**
- *  失败提醒
- *
- *  @param success 提醒文字
- *  @param color   颜色
- *  @param view    显示的view
- */
-+ (void)showError:(NSString *)error contentColor:(UIColor *)color inView:(UIView *)view;
-/**
- *  失败提醒
- *
- *  @param success 提醒文字
- *  @param view    显示的view
- */
-+ (void)showError:(NSString *)success inView:(UIView *)view;
-/**
- *  失败提醒
- *
- *  @param success 提醒文字
- *  @param color   颜色
- */
-+ (void)showError:(NSString *)success contentColor:(UIColor *)color;
-/**
- *  失败提醒
- *
- *  @param success 提醒文字
- */
-+ (void)showError:(NSString *)success;
-
-
-/**
- *  警告提醒
- *
- *  @param success 提醒文字
- *  @param color   颜色
- *  @param view    显示的view
- */
-+ (void)showInfo:(NSString *)info contentColor:(UIColor *)color inView:(UIView *)view;
-/**
- *  警告提醒
- *
- *  @param success 提醒文字
- *  @param view    显示的view
- */
-+ (void)showInfo:(NSString *)success inView:(UIView *)view;
-/**
- *  警告提醒
- *
- *  @param success 提醒文字
- *  @param color   颜色
- */
-+ (void)showInfo:(NSString *)success contentColor:(UIColor *)color;
-/**
- *  警告提醒
- *
- *  @param success 提醒文字
- */
-+ (void)showInfo:(NSString *)success;
-
-
-/**
- *  提醒
- *
- *  @param success 提醒文字
- *  @param color   颜色
- *  @param view    显示的view
- */
-+ (void)showMessage:(NSString *)message contentColor:(UIColor *)color inView:(UIView *)view;
-/**
- *  提醒
- *
- *  @param success 提醒文字
- *  @param view    显示的view
- */
-+ (void)showMessage:(NSString *)success inView:(UIView *)view;
-/**
- *  提醒
- *
- *  @param success 提醒文字
- *  @param color   颜色
- */
-+ (void)showMessage:(NSString *)success contentColor:(UIColor *)color;
-/**
- *  提醒
- *
- *  @param success 提醒文字
- */
-+ (void)showMessage:(NSString *)success;
-
-
 
 
 @end
--- FRDemo/FRDemo/FRCategory/MBProgressHUD/MBProgressHUD+Category.m
+++ FRDemo/FRDemo/FRCategory/MBProgressHUD/MBProgressHUD+Category.m
@@ -8,83 +8,36 @@
 
 #import "MBProgressHUD+Category.h"
 
+#define CricleAnimation @"cricleAnimation"
+
 @implementation MBProgressHUD (Category)
-
-
-+ (void)showTitle:(NSString *)text detailsLabel:(NSString *)detailText icon:(NSString *)icon contentColor:(UIColor *)color inView:(UIView *)view {
-    
-    if (view == nil) view = [[UIApplication sharedApplication].windows lastObject];
-    
-    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:view animated:YES];
-    
-    hud.contentColor = color;
-    hud.bezelView.color = [UIColor colorWithRed:192/255.0 green:192/255.0 blue:192/255.0 alpha:0.8];
-//    hud.minSize = CGSizeMake(160.f, 100.f);
-    
-    hud.label.text = text;
-   
-    if (icon.length > 0) {
-        hud.mode = MBProgressHUDModeCustomView;
-        hud.detailsLabel.text = detailText;
-        
-        NSBundle *bundle = [NSBundle bundleForClass:[MBProgressHUD class]];
-        NSURL *url = [bundle URLForResource:@"MBProgressHUD" withExtension:@"bundle"];
-        NSBundle *imageBundle = [NSBundle bundleWithURL:url];
-        NSString *path = [imageBundle pathForResource:icon ofType:@"png"];
-        UIImage *image = [[UIImage imageWithContentsOfFile:path] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
-        
-        hud.customView = [[UIImageView alloc] initWithImage:image];
-    }else {
-        hud.mode = MBProgressHUDModeText;
-    }
-//    hud.backgroundColor = [UIColor redColor];
-    
-    [hud hideAnimated:YES afterDelay:1.5];
-
-}
-
-
-+ (MBProgressHUD *)showTitle:(NSString *)text detailsLabel:(NSString *)detailText contentColor:(UIColor *)color progressHUDMode:(MBProgressHUDMode)progressHUDMode inView:(UIView *)view {
-    
-    if (view == nil) view = [[UIApplication sharedApplication].windows lastObject];
-    
-    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:view animated:YES];
-    
-    hud.contentColor = color;
-    hud.bezelView.color = [UIColor colorWithRed:192/255.0 green:192/255.0 blue:192/255.0 alpha:0.8];
-//    hud.minSize = CGSizeMake(160.f, 100.f);
-    
-    hud.label.text = text;
-    
-    hud.mode = progressHUDMode;
-    
-//    MBProgressHUDModeIndeterminate;系统默认
-//    MBProgressHUDModeAnnularDeterminate;//圆形加载
-//    MBProgressHUDModeDeterminateHorizontalBar;//水平加载
-//    MBProgressHUDModeDeterminate;//双环形圆行加载
-    
-    hud.detailsLabel.text = detailText;
-    
-    return hud;
-}
-
-+ (void)hideHUDForView:(UIView *)view
-{
-    if (view == nil) view = [[UIApplication sharedApplication].windows lastObject]; 
-    [self hideHUDForView:view animated:YES];
-}
-
-+ (void)hideHUD
-{
-    [self hideHUDForView:nil];
-}
 
 #pragma mark - 事件
 /**
  *  活动指示器
  */
 + (MBProgressHUD *)showActivityIndicatorWithTitle:(NSString *)title detailsTitle:(NSString *)detailTitle contentColor:(UIColor *)color progressHUDMode:(MBProgressHUDMode)progressHUDMode inView:(UIView *)view {
-    return [self showTitle:title detailsLabel:detailTitle contentColor:color progressHUDMode:progressHUDMode inView:view];
+    if (view == nil) view = [[UIApplication sharedApplication].windows lastObject];
+    
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:view animated:YES];
+    
+    hud.contentColor = color;
+    hud.bezelView.color = [UIColor colorWithRed:192/255.0 green:192/255.0 blue:192/255.0 alpha:0.8];
+    hud.minSize = CGSizeMake(90.f, 90.f);
+    
+    hud.label.text = title;
+    
+    hud.mode = progressHUDMode;
+    
+    //    MBProgressHUDModeIndeterminate;系统默认
+    //    MBProgressHUDModeAnnularDeterminate;//圆形加载
+    //    MBProgressHUDModeDeterminateHorizontalBar;//水平加载
+    //    MBProgressHUDModeDeterminate;//双环形圆行加载
+    
+    hud.detailsLabel.text = detailTitle;
+    
+    return hud;
+
 }
 
 + (MBProgressHUD *)showActivityIndicatorWithTitle:(NSString *)title detailsTitle:(NSString *)detailTitle progressHUDMode:(MBProgressHUDMode)progressHUDMode inView:(UIView *)view {
@@ -142,67 +95,185 @@
 
 
 #pragma mark - 提示状态
-//成功
-+ (void)showSuccess:(NSString *)success contentColor:(UIColor *)color inView:(UIView *)view {
+/**
+ *  带有自定义view的HUD
+ *
+ *  @param text       提醒标题
+ *  @param detailTitle 副标题
+ *  @param customView 自定义view
+ *  @param color      颜色
+ *  @param view       显示的view
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showTitle:(NSString *)text detailsLabel:(NSString *)detailTitle customView:(UIView *)customView contentColor:(UIColor *)color inView:(UIView *)view {
     
-    [self showTitle:success detailsLabel:nil icon:@"HUDsuccess" contentColor:color inView:view];
+    if (view == nil) view = [[UIApplication sharedApplication].windows lastObject];
+    
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:view animated:YES];
+    
+    hud.contentColor = color;
+    hud.bezelView.color = [UIColor colorWithRed:192/255.0 green:192/255.0 blue:192/255.0 alpha:0.8];
+    hud.minSize = CGSizeMake(90.f, 90.f);
+    
+    if(text) hud.label.text = text;
+    if(detailTitle) hud.detailsLabel.text = detailTitle;
+    
+    if (customView) {
+        hud.mode = MBProgressHUDModeCustomView;
+        
+        hud.customView = customView;
+    }else {
+        hud.mode = MBProgressHUDModeText;
+    }
+    
+    return hud;
 }
-+ (void)showSuccess:(NSString *)success inView:(UIView *)view {
-    [self showSuccess:success contentColor:[UIColor blackColor] inView:view];
+
+
+/**
+ *  带有图片的HUD
+ *
+ *  @param title      提醒标题
+ *  @param detailTitle 副标题
+ *  @param icon       图片
+ *  @param color      颜色
+ *  @param view       显示的view
+ *
+ *  @return MBProgressHUD对象
+ */
++ (MBProgressHUD *)showTitle:(NSString *)title detailsLabel:(NSString *)detailTitle icon:(NSString *)icon contentColor:(UIColor *)color inView:(UIView *)view {
+    
+    UIView *customView;
+    if (icon.length > 0) {
+        NSBundle *bundle = [NSBundle bundleForClass:[MBProgressHUD class]];
+        NSURL *url = [bundle URLForResource:@"MBProgressHUD" withExtension:@"bundle"];
+        NSBundle *imageBundle = [NSBundle bundleWithURL:url];
+        NSString *path = [imageBundle pathForResource:icon ofType:@"png"];
+        UIImage *image = [[UIImage imageWithContentsOfFile:path] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+        
+        customView = [[UIImageView alloc] initWithImage:image];
+    }
+    MBProgressHUD *hud = [self showTitle:title detailsLabel:detailTitle customView:customView contentColor:color inView:view];
+    return hud;
 }
-+ (void)showSuccess:(NSString *)success contentColor:(UIColor *)color {
-    [self showSuccess:success contentColor:color inView:nil];
+
+
+#pragma mark - 动画HUD
++ (MBProgressHUD *)showCustomeAnimationTitle:(NSString *)title detailsLabel:(NSString *)detailTitle icon:(NSString *)icon contentColor:(UIColor *)color inView:(UIView *)view {
+    
+    UIView *customView;
+    if (icon.length > 0) {
+        NSBundle *bundle = [NSBundle bundleForClass:[MBProgressHUD class]];
+        NSURL *url = [bundle URLForResource:@"MBProgressHUD" withExtension:@"bundle"];
+        NSBundle *imageBundle = [NSBundle bundleWithURL:url];
+        NSString *path = [imageBundle pathForResource:icon ofType:@"png"];
+        UIImage *image = [[UIImage imageWithContentsOfFile:path] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+        
+        customView = [[UIImageView alloc] initWithImage:image];
+        CABasicAnimation *anima = [CABasicAnimation animationWithKeyPath:@"transform.rotation"];
+        anima.toValue = @(M_PI*2);
+        anima.duration = 1.0f;
+        anima.repeatCount = MAXFLOAT;
+        [customView.layer addAnimation:anima forKey:CricleAnimation];
+    }
+    MBProgressHUD *hud = [self showTitle:title detailsLabel:detailTitle customView:customView contentColor:color inView:view];
+    return hud;
 }
-+ (void)showSuccess:(NSString *)success {
-    [self showSuccess:success contentColor:[UIColor blackColor] inView:nil];
+
++ (MBProgressHUD *)showCustomeAnimationTitle:(NSString *)title icon:(NSString *)icon contentColor:(UIColor *)color {
+    
+    return [self showCustomeAnimationTitle:nil detailsLabel:title icon:icon contentColor:color inView:nil];
+}
+
+
+#pragma mark - 常用提示状态
+
+//成功
++ (MBProgressHUD *)showSuccess:(NSString *)success contentColor:(UIColor *)color inView:(UIView *)view {
+    
+    MBProgressHUD *hud = [self showTitle:nil detailsLabel:success icon:@"HUDsuccess" contentColor:color inView:view];
+    [hud hideAnimated:YES afterDelay:1.5];
+    return hud;
+}
++ (MBProgressHUD *)showSuccess:(NSString *)success inView:(UIView *)view {
+    return [self showSuccess:success contentColor:[UIColor blackColor] inView:view];
+}
++ (MBProgressHUD *)showSuccess:(NSString *)success contentColor:(UIColor *)color {
+    return [self showSuccess:success contentColor:color inView:nil];
+}
++ (MBProgressHUD *)showSuccess:(NSString *)success {
+    return [self showSuccess:success contentColor:[UIColor blackColor] inView:nil];
 }
 
 
 //失败
-+ (void)showError:(NSString *)error contentColor:(UIColor *)color inView:(UIView *)view {
-    [self showTitle:error detailsLabel:nil icon:@"HUDerror" contentColor:color inView:view];
++ (MBProgressHUD *)showError:(NSString *)error contentColor:(UIColor *)color inView:(UIView *)view {
+    MBProgressHUD *hud = [self showTitle:nil detailsLabel:error icon:@"HUDerror" contentColor:color inView:view];
+    [hud hideAnimated:YES afterDelay:1.5];
+    return hud;
 }
-+ (void)showError:(NSString *)success inView:(UIView *)view {
-    [self showError:success contentColor:[UIColor blackColor] inView:view];
++ (MBProgressHUD *)showError:(NSString *)error inView:(UIView *)view {
+    return [self showError:error contentColor:[UIColor blackColor] inView:view];
 }
-+ (void)showError:(NSString *)success contentColor:(UIColor *)color {
-    [self showError:success contentColor:color inView:nil];
++ (MBProgressHUD *)showError:(NSString *)error contentColor:(UIColor *)color {
+    return [self showError:error contentColor:color inView:nil];
 }
-+ (void)showError:(NSString *)success {
-    [self showError:success contentColor:[UIColor blackColor] inView:nil];
++ (MBProgressHUD *)showError:(NSString *)error {
+    return [self showError:error contentColor:[UIColor blackColor] inView:nil];
 }
 
 
 //警告
-+ (void)showInfo:(NSString *)info contentColor:(UIColor *)color inView:(UIView *)view {
-    [self showTitle:info detailsLabel:nil icon:@"HUDinfo" contentColor:color inView:view];
++ (MBProgressHUD *)showInfo:(NSString *)info contentColor:(UIColor *)color inView:(UIView *)view {
+    MBProgressHUD *hud = [self showTitle:nil detailsLabel:info icon:@"HUDinfo" contentColor:color inView:view];
+    [hud hideAnimated:YES afterDelay:1.5];
+    return hud;
 }
-+ (void)showInfo:(NSString *)success inView:(UIView *)view {
-    [self showInfo:success contentColor:[UIColor blackColor] inView:view];
++ (MBProgressHUD *)showInfo:(NSString *)info inView:(UIView *)view {
+    return [self showInfo:info contentColor:[UIColor blackColor] inView:view];
 }
-+ (void)showInfo:(NSString *)success contentColor:(UIColor *)color {
-    [self showInfo:success contentColor:color inView:nil];
++ (MBProgressHUD *)showInfo:(NSString *)info contentColor:(UIColor *)color {
+    return [self showInfo:info contentColor:color inView:nil];
 }
-+ (void)showInfo:(NSString *)success {
-    [self showInfo:success contentColor:[UIColor blackColor] inView:nil];
++ (MBProgressHUD *)showInfo:(NSString *)info {
+    return [self showInfo:info contentColor:[UIColor blackColor] inView:nil];
 }
 
 
 //提醒
-+ (void)showMessage:(NSString *)message contentColor:(UIColor *)color inView:(UIView *)view {
-    [self showTitle:message detailsLabel:nil icon:nil contentColor:color inView:view];
++ (MBProgressHUD *)showMessage:(NSString *)message contentColor:(UIColor *)color inView:(UIView *)view {
+    MBProgressHUD *hud = [self showTitle:message detailsLabel:nil icon:nil contentColor:color inView:view];
+    [hud hideAnimated:YES afterDelay:1.5];
+    return hud;
 }
-+ (void)showMessage:(NSString *)success inView:(UIView *)view {
-    [self showMessage:success contentColor:[UIColor blackColor] inView:view];
++ (MBProgressHUD *)showMessage:(NSString *)message inView:(UIView *)view {
+    return [self showMessage:message contentColor:[UIColor blackColor] inView:view];
 }
-+ (void)showMessage:(NSString *)success contentColor:(UIColor *)color {
-    [self showMessage:success contentColor:color inView:nil];
++ (MBProgressHUD *)showMessage:(NSString *)message contentColor:(UIColor *)color {
+    return [self showMessage:message contentColor:color inView:nil];
 }
-+ (void)showMessage:(NSString *)success {
-    [self showMessage:success contentColor:[UIColor blackColor] inView:nil];
++ (MBProgressHUD *)showMessage:(NSString *)message {
+    return [self showMessage:message contentColor:[UIColor blackColor] inView:nil];
 }
 
 
+
+#pragma mark - 隐藏HUD
++ (void)hideHUDForView:(UIView *)view
+{
+    if (view == nil) view = [[UIApplication sharedApplication].windows lastObject];
+    MBProgressHUD *hud = [self HUDForView:view];
+    if (hud.customView) {
+        [hud.customView.layer removeAnimationForKey:CricleAnimation];
+    }
+    [self hideHUDForView:view animated:YES];
+}
+
++ (void)hideHUD
+{
+    [self hideHUDForView:nil];
+}
 
 
 
